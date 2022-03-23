11047c6bb020869c40e29072f567787b357674e2
tprayoga <tprayogaa26@gmail.com>
2022-03-24 01:12:52 +0700

    jss

--- form.js
+++ form.js
@@ -3,7 +3,7 @@ function submitData(){
     let email = document.getElementById('input-email').value
     let phone = document.getElementById('input-phone').value
     let subject = document.getElementById('input-subject').value
-    let message = document.getElementById('input-messege').value
+    let message = document.getElementById('input-message').value
 
     console.log(name);
     console.log(email);
